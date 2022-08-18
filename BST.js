110f96cddd2842892ad7071a460edb2a44b94d95
gl-cardillo <glucacardillo94@gmail.com>
2022-08-18 16:11:26 +0200

    add all methods

--- BST.js
+++ BST.js
@@ -19,15 +19,6 @@ class Tree {
         })
       ),
     ];
-
-    const arrayToBST = (array, start, end) => {
-      if (start > end) return null;
-      let mid = parseInt((start + end) / 2);
-      let root = new Node(array[mid]);
-      root.left = arrayToBST(array, start, mid - 1);
-      root.right = arrayToBST(array, mid + 1, end);
-      return root;
-    };
     return arrayToBST(sortedArray, 0, sortedArray.length - 1);
   }
 
@@ -37,7 +28,7 @@ class Tree {
       return root;
     }
     if (value < root.data) root.left = this.insert(value, root.left);
-    else if (value > root.data) root.right = this.insert(value, root.right);
+    if (value > root.data) root.right = this.insert(value, root.right);
     return root;
   }
 
@@ -53,8 +44,8 @@ class Tree {
     else if (value > root.data) root.right = this.delete(value, root.right);
     else {
       // node with only one child or no child
-      if (root.left == null) return root.right;
-      if (root.right == null) return root.left;
+      if (root.left === null) return root.right;
+      if (root.right === null) return root.left;
 
       // node with two children: Get the inorder
       // successor (smallest in the right subtree)
@@ -64,15 +55,98 @@ class Tree {
     }
     return root;
   }
+
+  find(value, root = this.root) {
+    if (root === null) {
+      return "Value not found";
+    }
+
+    if (root.data === value) {
+      return root;
+    }
+    if (value < root.data) return this.find(value, root.left);
+    else if (value > root.data) return this.find(value, root.right);
+  }
+
+  levelOrder(root) {
+    if (root === null) return;
+    let result = [];
+    let queue = [root];
+
+    while (queue.length > 0) {
+      let current = queue.shift(root);
+      result.push(current.data);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return result;
+  }
+
+  preorder(root, result = []) {
+    if (root === null) return;
+    result.push(root.data);
+    if (root.left) this.preorder(root.left, result);
+    if (root.right) this.preorder(root.right, result);
+    return result;
+  }
+
+  inorder(root, result = []) {
+    if (root === null) return;
+    if (root.left) this.inorder(root.left, result);
+    result.push(root.data);
+    if (root.right) this.inorder(root.right, result);
+    return result;
+  }
+
+  postorder(root, result = []) {
+    if (root === null) return;
+    if (root.left) this.postorder(root.left, result);
+    if (root.right) this.postorder(root.right, result);
+    result.push(root.data);
+    return result;
+  }
+
+  height(root) {
+    if (root === null) return -1;
+    let left = this.height(root.left);
+    let right = this.height(root.right);
+    return Math.max(left, right) + 1;
+  }
+
+  isBalanced(root) {
+    if (root === null) return false;
+    if (Math.abs(this.height(root.left) - this.height(root.right)) > 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  rebalance() {
+    if (this.isBalanced(this.root)) return this.root;
+    let array = this.preorder(this.root);
+    let balancedTree = new Tree(array);
+    console.log(balancedTree);
+    return balancedTree;
+  }
 }
 
-function minValue(root) {
+const arrayToBST = (array, start, end) => {
+  if (start > end) return null;
+  let mid = parseInt((start + end) / 2);
+  let root = new Node(array[mid]);
+  root.left = arrayToBST(array, start, mid - 1);
+  root.right = arrayToBST(array, mid + 1, end);
+  return root;
+};
+
+const minValue = (root) => {
   let min = root.data;
   while (root != null) {
     min = root.data;
     root = root.left;
   }
   return min;
-}
+};
 
 export { Tree, Node };
--- BST.test.js
+++ BST.test.js
@@ -55,7 +55,7 @@ describe("Create tree", () => {
 });
 
 describe("methods of the binary search tree", () => {
-  it("should insert a new value in the tree", () => {
+  it("insert() should insert a new value in the tree", () => {
     expect(tree.insert(11)).toEqual(
       expect.objectContaining({
         data: 8,
@@ -109,7 +109,7 @@ describe("methods of the binary search tree", () => {
       })
     );
   });
-  it("it should delete the value in the tree", () => {
+  it("delete() should delete the value in the tree", () => {
     expect(tree.delete(11)).toEqual(
       expect.objectContaining({
         data: 8,
@@ -158,5 +158,128 @@ describe("methods of the binary search tree", () => {
         },
       })
     );
+  });
+  it("find() should return value not found it there is no value", () => {
+    expect(tree.find(11)).toBe("Value not found");
+  });
+  it("find() should find a value in the tree", () => {
+    expect(tree.find(9)).toEqual(
+      expect.objectContaining({
+        data: 9,
+        left: null,
+        right: { data: 23, left: null, right: null },
+      })
+    );
+  });
+
+  it("levelOrder() should traverse the tre in breadth-first level order", () => {
+    expect(tree.levelOrder(tree.root)).toEqual([
+      8, 4, 67, 1, 5, 9, 324, 3, 7, 23, 6345,
+    ]);
+  });
+
+  it("preorder() should traverse the tre in depth-first preorder", () => {
+    expect(tree.preorder(tree.root)).toEqual([
+      8, 4, 1, 3, 5, 7, 67, 9, 23, 324, 6345,
+    ]);
+  });
+
+  it("inorder() should traverse the tre in depth-first inorder", () => {
+    expect(tree.inorder(tree.root)).toEqual([
+      1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345,
+    ]);
+  });
+
+  it("postorder() should traverse the tre in depth-first postorder", () => {
+    expect(tree.postorder(tree.root)).toEqual([
+      3, 1, 7, 5, 4, 23, 9, 6345, 324, 67, 8,
+    ]);
+  });
+
+  it("height() should return the height (longest path from a given node to a leaf node)", () => {
+    expect(tree.height(tree.root)).toBe(3);
+    expect(tree.height(tree.root.right)).toBe(2);
+  });
+
+  it("height() should return -1 if the tree is empty", () => {
+    expect(tree.height(null)).toBe(-1);
+  });
+
+  it("isBalanced() should return true if the tree is balanced", () => {
+    expect(tree.isBalanced(tree.root)).toBeTruthy();
+  });
+
+  test("isBalanced() returns false for unbalanced tree", () => {
+    tree.insert(5555);
+    tree.insert(5556);
+    tree.insert(5557);
+    expect(tree.isBalanced(tree.root)).toBeFalsy();
+  });
+
+  it("rebalance() should rebalance an unbalanced tree", () => {
+    expect(tree.rebalance()).toEqual(
+      expect.objectContaining({
+        root: {
+          data: 9,
+          left: {
+            data: 4,
+            left: {
+              data: 1,
+              left: null,
+              right: {
+                data: 3,
+                left: null,
+                right: null,
+              },
+            },
+            right: {
+              data: 7,
+              left: {
+                data: 5,
+                left: null,
+                right: null,
+              },
+              right: {
+                data: 8,
+                left: null,
+                right: null,
+              },
+            },
+          },
+          right: {
+            data: 5555,
+            left: {
+              data: 67,
+              left: {
+                data: 23,
+                left: null,
+                right: null,
+              },
+              right: {
+                data: 324,
+                left: null,
+                right: null,
+              },
+            },
+            right: {
+              data: 5557,
+              left: {
+                data: 5556,
+                left: null,
+                right: null,
+              },
+              right: {
+                data: 6345,
+                left: null,
+                right: null,
+              },
+            },
+          },
+        },
+      })
+    );
+
+    // console.log(tree)
+    // expect(tree.isBalanced(tree.root)).toBeTruthy();
   });
 });
